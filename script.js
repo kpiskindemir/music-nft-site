18927d29b4d87da10d637254c30b2fd60e15ca9c
kpiskindemir <129279571+kpiskindemir@users.noreply.github.com>
2024-06-02 10:51:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const uploadForm = document.getElementById('uploadForm');
+  const accountForm = document.getElementById('accountForm');
+
+  uploadForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // NFT oluşturma işlemleri burada yapılacak
+    alert('NFT Oluşturuldu!');
+  });
+
+  accountForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // Hesap oluşturma ve cüzdan bağlama işlemleri burada yapılacak
+    alert('Hesap oluşturuldu ve cüzdan bağlandı!');
+  });
+});
